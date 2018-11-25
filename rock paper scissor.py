c50403493920a85e36b974cef208d702c1ed12a7
BBloggsbott <vishal27.rs@gmail.com>
2018-11-25 14:02:45 +0530

    Added Draw case

--- rock paper scissor.py
+++ rock paper scissor.py
@@ -1,44 +1,43 @@
 import random
 play=True
 while(play):
-    print('select rock(r) , paper (p), scissor (s)  ')
-    choice=input('enter your choice  (r),(p),(s):  ' )
-    if choice=='r':
-        print('you have selected rock(r)')
-    elif choice =='p':
-        print('you have selected paper(p)')
-    elif choice=='s':
-        print('you have selected scissor(s)')
-    else:
-        print('wrong option')
-    print('lets play')
-    print('you vs machine')
-    if choice=='r':
-        machine=random.choice(['s','p'])
-        if machine =='s':
-            print(choice+' v '+machine+ '      you won ')
-        else:
-            print(choice+' v '+machine +'      machine won ')
-    elif choice=='s':
-        machine=random.choice(['r','p'])
-        if machine =='r':
-            print(choice+' v '+machine + '     machine won ')
-        else:
-            print(choice+' v '+machine + '     you wone ')
-    elif choice=='p':
-        machine=random.choice(['s','r'])
-        if machine =='s':
-            print(choice+' v '+machine + '     machine won')
-        else:
-            print(choice+' v '+machine +'      you won ')
-    
-    print("**************Game over**************")
+	print('--\nSelect rock(r) , paper (p), scissor (s)  ')
+	choice=input('enter your choice  (r|p|s):  ' )
+	if choice=='r':
+		print('you have selected rock(r)')
+	elif choice =='p':
+		print('you have selected paper(p)')
+	elif choice=='s':
+		print('you have selected scissor(s)')
+	else:
+		print('wrong option')
+		continue
+	print('lets play')
+	print('you vs machine')
+	machine = random.choice(['r', 'p', 's'])
+	if choice=='r':
+		if machine =='s':
+		    print(choice+' v '+machine+ '\tYou won ')
+		elif machine == 'p':
+		    print(choice+' v '+machine +'\tMachine won ')
+		else:
+			print(choice+' v '+machine +'\tDraw ')
+	elif choice=='s':
+		if machine =='r':
+		    print(choice+' v '+machine + '\tMachine won ')
+		elif machine == 'p':
+		    print(choice+' v '+machine + '\tYou wone ')
+		else:
+		    print(choice+' v '+machine +'\tDraw ')
+	elif choice=='p':
+		if machine =='s':
+		    print(choice+' v '+machine + '\tMachine won')
+		elif machine == 'r':
+		    print(choice+' v '+machine +'\tYou won ')
+		else:
+		    print(choice+' v '+machine +'\tDraw ')
+	print("**************Game over**************")
 
-    yes=input('do you want to play again ( YES /NO )')
-    if yes=='no':
-        play=False
-
-
-    
-    
-
+	yes=input('do you want to play again ([YES]/NO)')
+	if yes.lower()=='no':
+		play=False
